f58463299cd80108251c986fe8d09560e8766041
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-11-20 14:08:38 -0800

    Delete index.js
    Not sure why it's saying this is original. I pulled it from the repo. Deleting just in case.

--- controllers/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const apiRoutes = require('../api');
-const homeRoutes = require('./homeroutes');
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;
