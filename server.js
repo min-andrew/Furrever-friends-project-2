0166f96d178ef4eee85a09a229702b88992ed3f0
Kenny Wai <97656734+KennyWCoder@users.noreply.github.com>
2022-11-21 19:38:35 -0800

    server.js basic setup will need a new npm add-on when we decide which one to use

---
+++ server.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const hb = require('express-handlers');
+const apiRoutes = require('./controllers');
+const util = require('./utils/helpers');
+//deciding a npm to use
+//const doggy = require('')
+
+//sequlize connection
+const sequelize = require('./config/connection');
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//setup express and port with env(secured)
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbjs = hb.create({ util })
+
+const cookie = {
+    secret: 'browser history cookie',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new sequelizeStore({
+      db: sequelize
+    })
+  };
+  
+app.use(session(cookie));
+
+app.engine('handlebars', hbjs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(apiRoutes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
