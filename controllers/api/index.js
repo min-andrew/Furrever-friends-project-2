752903c86dec8144352fe50dd397f456d60c70ff
Andrew Min <minandrew0711@gmail.com>
2022-11-23 20:33:39 -0800

    corrected index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./postRoutes');
+const postRoutes = require('./postRoutes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
