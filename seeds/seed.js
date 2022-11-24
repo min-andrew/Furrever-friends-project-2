db8e6b85d610f3ce3b2645e475823f92698157e6
Andrew Min <minandrew0711@gmail.com>
2022-11-23 19:48:16 -0800

    changed the seeds file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models/Index');
+const { User, Post } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
