8f25efde2837c4be6801d4eb6519d4f2d0f499f8
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-11-27 21:48:13 -0800

    Fixed the routes so now the login, sign up, and viewing posts works.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,8 +2,7 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// These routes create, update, or delete posts
-
+// Routes to create new posts or delete posts
 router.post('/', withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
@@ -14,25 +13,6 @@ router.post('/', withAuth, async (req, res) => {
     res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
-  }
-});
-
-router.put('/:id', withAuth, async (req, res) => {
-  try {
-    const postData = await Post.update( req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-
-    if (!postData) {
-      res.status(404).json({ message: 'No post found with this id!' });
-      return;
-    }
-
-    res.status(200).json(postData);
-  } catch (err) {
-    res.status(500).json(err);
   }
 });
 
