5a5aadb7b1abcf5b16660df93e6f5348760a63e2
Tracy Pham <phamtracy95@gmail.com>
2022-11-28 20:08:11 -0800

    Connected profile handlebars to profile public JS so that the create profile form works

--- public/JS/profile.js
+++ public/JS/profile.js
@@ -1,29 +1,19 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const displayName = document.querySelector("#display-name").value.trim();
-  const dogName = document.querySelector("#dog-name").value.trim();
-  const breed = document.querySelector("#breed").value.trim();
-  const gender = document.querySelector("#gender").value.trim();
-  const size = document.querySelector("#size").value.trim();
-  const neighborhood = document.querySelector("#neighborhood").value.trim();
-  const hobbies = document.querySelector("#hobbies").value.trim();
+  const dog_name = document.querySelector("#dog-name").value.trim();
+  const breed = document.querySelector("#dog-breed").value.trim();
+  const gender = document.querySelector("#dog-gender").value.trim();
+  const size = document.querySelector("#dog-size").value.trim();
+  const neighborhood = document.querySelector("#dog-neighborhood").value.trim();
+  const hobbies = document.querySelector("#dog-hobbies").value.trim();
 
   // If all profile fields are answered, then create a new profile
-  if (
-    displayName &&
-    dogName &&
-    breed &&
-    gender &&
-    size &&
-    neighborhood &&
-    hobbies
-  ) {
-    const response = await fetch(`/api/profiles`, {
+  if (dog_name && breed && gender && size && neighborhood && hobbies) {
+    const response = await fetch("/api/profile", {
       method: "POST",
       body: JSON.stringify({
-        displayName,
-        dogName,
+        dog_name,
         breed,
         gender,
         size,
