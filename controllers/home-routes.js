49b68c0b31bd2e21be7ae2f0660d1e07e5dcfdec
Tracy Pham <phamtracy95@gmail.com>
2022-12-01 13:22:55 -0800

    Added associations for single post page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -94,7 +94,7 @@ router.get("/profile", withAuth, async (req, res) => {
     res.render("profile", {
       ...user,
       logged_in: true,
-      user_id: req.session.user_id
+      user_id: req.session.user_id,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -130,7 +130,7 @@ router.get("/profile/:id", async (req, res) => {
     res.render("viewProfile", {
       profile,
       logged_in: true,
-      user_id: req.session.user_id
+      user_id: req.session.user_id,
     });
   } catch (err) {
     res.status(500).json(err);
