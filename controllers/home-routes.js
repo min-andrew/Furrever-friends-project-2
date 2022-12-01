725ba8fa4be92ac7c77d7591b70c34f51f1f9c2a
Tracy Pham <phamtracy95@gmail.com>
2022-11-30 20:34:38 -0800

    Added avatars to post page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,17 +17,18 @@ router.get("/post", (req, res) => {
   Post.findAll({
     include: [
       {
-        model: User, 
-        attributes: ['name', 'id']
+        model: User,
+        attributes: ["name", "id"],
+        include: { model: Profile, attributes: ["avatar"] },
       },
       {
-        model: Comment
-      }
-    ]
+        model: Comment,
+      },
+    ],
   })
     .then((postData) => {
       const posts = postData.map((post) => post.get({ plain: true }));
-      console.log(posts)
+      console.log(posts);
       res.render("post", { posts, logged_in: req.session.logged_in });
     })
     .catch((err) => {
@@ -36,52 +37,37 @@ router.get("/post", (req, res) => {
 });
 
 // GET route for single post and comments
-router.get('/post/:id', (req, res) => {
+router.get("/post/:id", (req, res) => {
   Post.findOne({
     where: {
-      id: req.params.id
+      id: req.params.id,
     },
-    attributes: [
-      'id',
-      'title',
-      'image',
-      'imageId',
-      'body',
-      'date_created'
-    ],
+    attributes: ["id", "title", "image", "imageId", "body", "date_created"],
     include: [
       {
         model: Comment,
-        attributes: [
-          'id', 
-          'comment_body', 
-          'post_id', 
-          'user_id', 
-        ],
+        attributes: ["id", "comment_body", "post_id", "user_id"],
         include: {
           model: User,
-          attributes: [
-            'name',
-            'id'
-          ]
-        }
+          attributes: ["name", "id"],
+        },
       },
-    ]
+    ],
   })
-    .then(postData => {
+    .then((postData) => {
       if (!postData) {
-        res.status(404).json({ message: 'No post found with this id!' });
+        res.status(404).json({ message: "No post found with this id!" });
         return;
       }
 
       const post = postData.get({ plain: true });
 
-      res.render('singlepost', {
-          post,
-          logged_in: req.session.logged_in
-        });
+      res.render("singlepost", {
+        post,
+        logged_in: req.session.logged_in,
+      });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
@@ -89,7 +75,7 @@ router.get('/post/:id', (req, res) => {
 
 // get route for profile with auth
 router.get("/profile", withAuth, async (req, res) => {
-  console.log("here")
+  console.log("here");
   try {
     // find logged user based on session id
     const userData = await User.findByPk(req.session.user_id, {
@@ -118,19 +104,22 @@ router.get("/createac", (req, res) => {
 
 router.get("/profile/:id", async (req, res) => {
   try {
-    const profileData = await Profile.findOne({where: {user_id: req.params.id }}, {
-      include: [
-        {
-          model: User,
-          attributes: ["name"],
-        },
-      ],
-    });
+    const profileData = await Profile.findOne(
+      { where: { user_id: req.params.id } },
+      {
+        include: [
+          {
+            model: User,
+            attributes: ["name"],
+          },
+        ],
+      }
+    );
 
     const profile = profileData.get({ plain: true });
 
-    console.log("profile:", profile)
-   
+    console.log("profile:", profile);
+
     res.render("viewProfile", {
       profile,
       logged_in: true,
